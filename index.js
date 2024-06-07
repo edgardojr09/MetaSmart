c8c8e9007b5c686e8907c7061e3d9ef7418ae325
edgardojr09 <128987307+edgardojr09@users.noreply.github.com>
2024-06-07 23:48:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
+import { redirect } from 'next/dist/server/api-utils';
 
 const BeHealthy = () => {
+  
   const [water, setWater] = useState('');
   const [sleep, setSleep] = useState('');
   const [eat, setEat] = useState('');
   const [message, setMessage] = useState('');
   const [account, setAccount] = useState(null);
+  const [error, setError] = useState("");
+  const [shows, setShow] = useState("Connect MetaMask");
 
   const handleWaterChange = (e) => setWater(e.target.value);
   const handleSleepChange = (e) => setSleep(e.target.value);
   const handleEatChange = (e) => setEat(e.target.value);
 
   const checkWaterIntake = () => {
-    try {
-      if (parseInt(water) !== 8) {
-        throw new Error('You need 8 glasses of water per day to be healthy');
+      if(message == ""){
+        setError("You can't show the value if you didnt connect to the metamask first!");
       }
+      else {
+        if (parseInt(water) !== 8) {
+        setMessage('You need 8 glasses of water per day to be healthy');
+        setError('');
+      }else{
       setMessage('Correct water intake!');
-    } catch (error) {
-      setMessage(error.message);
+      }
     }
   };
 
   const checkSleep = () => {
-    try {
-      if (parseInt(sleep) !== 8) {
-        throw new Error('You need 8 hours sleep to be healthy');
-      }
-      setMessage('Correct sleep duration!');
-    } catch (error) {
-      setMessage(error.message);
+    if(message == ""){
+      setError("You can't show the value if you didnt connect to the metamask first!");
     }
+    else {
+      if (parseInt(sleep) !== 8) {
+      setMessage('You need 8 hours sleep to be healthy');
+      setError('');
+    }else{
+    setMessage('Correct sleep duration!');
+    }
+  }
   };
 
   const checkEatTimes = () => {
-    try {
-      const eatTimes = parseInt(eat);
-      if (eatTimes !== 3) {
-        throw new Error('You need to eat 3 times a day to be healthy');
-      }
-      setMessage('Correct eating frequency!');
-    } catch (error) {
-      setMessage(error.message);
+    if(message == ""){
+      setError("You can't show the value if you didnt connect to the metamask first!");
     }
+    else {
+      if (parseInt(eat) !== 3) {
+      setMessage('You need eat 3 times a day to be healthy');
+      setError('');
+    }else{
+    setMessage('Correct eating frequency!');
+    }
+  }
   };
 
   const connectWallet = async () => {
@@ -54,7 +66,9 @@ const BeHealthy = () => {
         const signer = provider.getSigner();
         const userAddress = await signer.getAddress();
         setAccount(userAddress);
-        setMessage(`Connected to MetaMask: ${userAddress}`);
+        setError('');
+        setShow("Show Wallet Address");
+        setMessage("Connected Successfully: " + userAddress);
       } else {
         setMessage('MetaMask is not installed');
       }
@@ -106,6 +120,9 @@ const BeHealthy = () => {
       marginTop: '20px',
       fontSize: '1.2em',
     },
+    error:{
+      color: 'red',
+    },
   };
 
   return (
@@ -134,11 +151,12 @@ const BeHealthy = () => {
           <input type="number" value={eat} onChange={handleEatChange} style={styles.input} />
         </label>
         <button onClick={checkEatTimes} style={styles.button}>Check Eating Frequency</button>
-      </div>
+      </div><br />
 
-      <button onClick={connectWallet} style={styles.button}>Connect MetaMask</button>
+      <button onClick={connectWallet} style={styles.button}>{shows}</button>
 
       {message && <p style={styles.message}>{message}</p>}
+      {error && <p style={styles.error}>{error}</p>}
     </div>
   );
 };
